469bd5f0c10113ab282e26f197fe2769bd7a1225
Gabriel Bento Lisboa <gabrielbento.rx@outlook.com>
2019-07-17 08:43:45 -0300

    Using immer on reducers

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
       }
     ],
     'import/prefer-default-export': 'off',
-    'no-console': ["error", {allow: ['tron']}]
+    'no-console': ["error", {allow: ['tron']}],
+    'no-param-reassign': 'off'
   },
 };
